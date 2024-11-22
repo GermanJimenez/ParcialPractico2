0ea93542192dc00e316062572b153bc7f3c84af6
GermanJimenez <gd.jimenez@uniandes.edu.co>
2024-11-22 12:21:23 -0500

    routing

--- src/app/anime/anime.module.ts
+++ src/app/anime/anime.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimeListComponent } from './anime-list/anime-list.component';
 import { AnimeDetailComponent } from './anime-detail/anime-detail.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    AppRoutingModule
   ],
+  
   exports: [AnimeListComponent],
   declarations: [AnimeListComponent, AnimeDetailComponent]
 })
